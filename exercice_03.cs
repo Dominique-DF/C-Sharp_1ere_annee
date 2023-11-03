40e3a3c592c6e466e6dca70322dcd81909d6a2d1
Dominique-DF <120120747+Dominique-DF@users.noreply.github.com>
2023-11-03 14:15:43 +0100

    Add files via upload

---
+++ exercice_03.cs
@@ -0,0 +1,12 @@
+﻿// Exercice 3
+int annee;
+Console.WriteLine("Entrez une année : ");
+annee = int.Parse(Console.ReadLine());
+if (annee % 4 == 0 && annee % 100 != 0 || annee % 400 == 0)
+{
+    Console.WriteLine("L'année {0} est bissextile", annee);
+}
+else
+{
+    Console.WriteLine("L'année {0} n'est pas bissextile", annee);
+}
