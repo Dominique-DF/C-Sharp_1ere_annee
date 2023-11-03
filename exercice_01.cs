7458b9885e6d01a7eceace1b99cfac7f5f02df46
Dominique-DF <120120747+Dominique-DF@users.noreply.github.com>
2023-11-03 14:12:48 +0100

    Add files via upload

---
+++ exercice_01.cs
@@ -0,0 +1,17 @@
+﻿// Exercice 1
+int annee_actuelle, annee_naissance, age;
+Console.Write("Année actuelle : ");
+annee_actuelle = int.Parse(Console.ReadLine());
+Console.Write("Année de naissance : ");
+annee_naissance = int.Parse(Console.ReadLine());
+age = annee_actuelle - annee_naissance;
+if (age>=18)
+{
+    Console.WriteLine("Vous êtes majeur");
+}
+else
+{
+    Console.WriteLine("Vous êtes mineur");
+    Console.WriteLine("Il vous reste {0} ans avant la majorité", 18 - age);
+}
+
