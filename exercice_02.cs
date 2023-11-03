7a9d5caca0ed4a5a437782437bf40a96958acb16
Dominique-DF <120120747+Dominique-DF@users.noreply.github.com>
2023-11-03 14:16:18 +0100

    Add files via upload

---
+++ exercice_02.cs
@@ -0,0 +1,9 @@
+﻿// Exercice 2
+double note;
+Console.WriteLine("Entrez une note : ");
+note = double.Parse(Console.ReadLine());
+while (note < 0 || note > 20)
+{
+    Console.WriteLine("Entrez une note valide : ");
+    note = double.Parse(Console.ReadLine());
+}
