137e5479736b5e92519df57f097d121c12f38274
Dominique-DF <120120747+Dominique-DF@users.noreply.github.com>
2023-11-03 15:33:41 +0100

    Add files via upload

---
+++ exercice_05.cs
@@ -0,0 +1,27 @@
+﻿// Exercice 4
+int heureDepart, minDepart, heureArrivee, minArrivee, heureTrajet, minTrajet;
+
+Console.Write("Heure de départ : ");
+heureDepart = int.Parse(Console.ReadLine());
+Console.Write("Minute de départ : ");
+minDepart = int.Parse(Console.ReadLine());
+Console.Write("Heure d'arrivée : ");
+heureArrivee = int.Parse(Console.ReadLine());
+Console.Write("Minute d'arrivée : ");
+minArrivee = int.Parse(Console.ReadLine());
+
+if (heureArrivee < heureDepart)
+{
+    heureArrivee += 24;
+}
+
+heureTrajet = heureArrivee - heureDepart;
+minTrajet = minArrivee - minDepart;
+
+if (minTrajet < 0)
+{
+    minTrajet += 60;
+    heureTrajet--;
+}
+
+Console.WriteLine($"Le trajet a duré {heureTrajet} heures et {minTrajet} minutes.");
