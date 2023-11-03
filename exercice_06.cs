45d02a9c723e8ca445fd3060046d5fe172bb85e3
Dominique-DF <120120747+Dominique-DF@users.noreply.github.com>
2023-11-03 15:36:15 +0100

    Add files via upload

---
+++ exercice_06.cs
@@ -0,0 +1,11 @@
+﻿// Exercice 6
+int nombre;
+
+Console.WriteLine("Entrez un nombre : ");
+nombre = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{nombre} x {i} = {nombre * i}");
+    // OU Console.WriteLine(nombre + " x " + i + " = " + (nombre * i));
+}
