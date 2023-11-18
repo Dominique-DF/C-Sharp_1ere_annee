8c2c77ec7e298a896f0d496c998ca9d92822266a
Dominique-DF <120120747+Dominique-DF@users.noreply.github.com>
2023-11-18 19:33:21 +0100

    Add files via upload

---
+++ exercice_09.cs
@@ -0,0 +1,28 @@
+﻿// Exercice 9
+
+int max=0, min=0, nb;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("Entrez un nombre : ");
+    nb = int.Parse(Console.ReadLine());
+    if (i == 0)
+    {
+        max = nb;
+        min = nb;
+    }
+    else
+    {
+        if (nb > max)
+        {
+            max = nb;
+        }
+        if (nb < min)
+        {
+            min = nb;
+        }
+    }
+}
+
+Console.WriteLine("Le plus grand nombre est : " + max);
+Console.WriteLine("Le plus petit nombre est : " + min);
