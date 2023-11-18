5cbe2295f0989821234938482a2178a4f805ac6d
Dominique-DF <120120747+Dominique-DF@users.noreply.github.com>
2023-11-18 19:37:13 +0100

    Add files via upload

---
+++ exercice_10.cs
@@ -0,0 +1,34 @@
+﻿// Exercice 10
+
+int max = 0, min = 0, nb;
+bool premierNbre = true;
+string saisie;
+
+do
+{
+    Console.Write("Entrez un nombre ou FIN : ");
+    saisie = Console.ReadLine();
+    if (saisie != "FIN")
+    {
+        nb = int.Parse(saisie);
+
+        if (premierNbre)
+        {
+            max = nb;
+            min = nb;
+            premierNbre = false;
+        }
+
+        if (nb > max)
+        {
+            max = nb;
+        }
+        else if (nb < min)
+        {
+            min = nb;
+        }
+    }
+} while (saisie != "FIN");
+
+Console.WriteLine($"Le plus grand nombre est {max}");
+Console.WriteLine($"Le plus petit nombre est {min}");
